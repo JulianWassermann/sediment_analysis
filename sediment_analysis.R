cc677d7487e27ce3f1347498ed9b3588fca4f1fc
Julian Wassermann <jw4251@york.ac.uk>
2024-06-14 03:43:17 +0100

    Sediment_analysis.R version 6: Major changes

--- sediment_analysis.R
+++ sediment_analysis.R
@@ -7,110 +7,109 @@ suppressPackageStartupMessages({
   library(tidyr)
   library(patchwork)
   library(readxl)
+  library(qtl)
 })
+# you will need more libraries than those above
 
-# Main body, use call command, figure out how to return a function
-# given a plot, work out its limits
-get_plot_limits <- function(data_exel) {
-  # Build ggplot object for the data sets
-  min_vale <- min(data_exel)
+# given a plot, work out it's limits
+get_plot_limits<-function(data_exel) {
+  
+  #
+  data_exel <- select(data_exel, -sample_depth_cm)
+  #Minimum and maximum values in core_16
+  min_value <- min(data_exel)
   max_value <- max(data_exel)
+  limits <- list(min_value, max_value)
+} 
+
+read_sed_data<-function(input_file) {
+  # Read the Excel file and return the data
+  sed_data <- read_excel(input_file)
+  return(sed_data)
   
-  # Return min and max values
-  return(min_value, max_value)
+  
+  # Define the path to the input file
+  input_file <- "Raw Data/core_16.xlsx"
+  sed_data <- read_sed_data(input_file)
+  print(head(sed_data))
 }
 
-read_sed_data <- function(depth_file) {
-  # Check if the core_16 is a string
-  if (!is.character(core_16)) {
-    stop("Error: `core_16` must be a string")
-  }
-  
-  # Implement logic to read sediment data from file
-  data <- read_excel(core_16)
-  return(data)
-}
 
 read_core_depths <- function(depth_file) {
-  # Implement logic to read core depth data from file
-  data <- read.csv(depth_file)
-  return(data)
+  #Implement logic to read core depth data from file
+  read.csv(depth_file)
 }
 
-#calculate_grain_percentages <- function(grain_sizes, grain_stats, gs_data_matrix) {
-#Placeholder for the actual implementation
-#}
+calculate_grain_percentages<-function(grain_sizes, grain_stats, gs_data_matrix) {
+  
+  Silt_data <- rowSums(sed_data[ , c(2:64)])
+  VCSilt_data <- rowSums(sed_data[ , c(65:70)])
+  VFS_data <- rowSums(sed_data[ , c(71:76)])
+  FS_data <- rowSums(sed_data[ , c(77:82)])
+  MS_data <- rowSums(sed_data[ , c(83:88)])
+  CS_data <- rowSums(sed_data[ , c(89:94)])
+  VCS_data <- rowSums(sed_data[ , c(95:100)])
+  VFG_data <- rowSums(sed_data[ , c(101)])
+  
+  
+  sed_data_percentages <- data.frame(Silt = c(Silt_data),
+                                     VCSilt = c(VCSilt_data),
+                                     VFS = c(VFS_data),
+                                     FS = c(FS_data),
+                                     MS = c(MS_data),
+                                     CS = c(CS_data),
+                                     VCS = c(VCS_data),
+                                     VFG = c(VFG_data))
+}
 
 main <- function(args) {
+  
+  #
+  sed_data <- read_sed_data("Raw Data/core_16.xlsx")
+  
+  #
+  Limits <- get_plot_limits(sed_data)
+  
+  #
+  min_value <- Limits[[1]]
+  max_value <- Limits[[2]]
+  
   if (is.null(args$depth_data)) {
     # default depths file
-    depth_file <- "depth_data.csv"
+    depth_file <- "depth_data (1).csv"
+    #
+    depth_data <- read_core_depths(depth_file)
   } else {
     depth_file <- args$depth_data 
   }
   
-  #grain_stats <- granstat(gs_data_matrix)
+  #
+  sed_data_percentages <- calculate_grain_percentages(sed_data)
   
-  # For percentage diagram
-  plot1 <- ggplot(grain_stats, aes(x=x, y=y)) + geom_bar(stat = "identity") + xlim(25, 35) + ylim(0, 1)
-  limits1 <- get_plot_limits()
-  print("Plot 1 Limits (Percentage Diagram):")
-  print(limits1)
+  colsum
+  # now use the gradistats package to get the mean, kurtosis, etc and the % 
+  # in each standard grain size bucket
+  grain_stats <- granstat(gs_data_matrix)
   
-  # For mean GS (mm) diagram
-  plot2 <- ggplot(grain_stats, aes(x=x, y=y)) + geom_line() + xlim(25, 35) + ylim(0, 1.25)
-  limits2 <- get_plot_limits()
-  print("Plot 2 Limits (Mean GS (mm) Diagram):")
-  print(limits2)
+  # set-up the order in which we want grainsizes to be displayed (and which we want to be displayed)
+  grain_sizes <- c("Silt",
+                   "VCSilt",
+                   "VFS",
+                   "FS",
+                   "MS",
+                   "CS",
+                   "VCS",
+                   "VFG"
+  )
+  # Note, leaving off coarse grains
   
-  # For kurtosis diagram
-  plot3 <- ggplot(grain_stats, aes(x=x, y=y)) + geom_line() + xlim(25, 35) + ylim(0, 10)
-  limits3 <- get_plot_limits()
-  print("Plot 3 Limits (Kurtosis Diagram):")
-  print(limits3)
-  
-  # For sorting diagram
-  plot4 <- ggplot(grain_stats, aes(x=x, y=y)) + geom_line() + xlim(25, 35) + ylim(0, 10)
-  limits4 <- get_plot_limits()
-  print("Plot 4 Limits (Sorting Diagram):")
-  print(limits4)
-  
-  # For skew diagram
-  plot5 <- ggplot(grain_stats, aes(x=x, y=y)) + geom_line() + xlim(25, 35) + ylim(0, 10)
-  limits5 <- get_plot_limits()
-  print("Plot 5 Limits (Skew Diagram):")
-  print(limits5)
-  
-  # Plotting Diagrams
-  ggsave("plot1.pdf", plot=plot1)
-  ggsave("plot2.pdf", plot=plot2)
-  ggsave("plot3.pdf", plot=plot3)
-  ggsave("plot4.pdf", plot=plot4)
-  ggsave("plot5.pdf", plot=plot5)
+  # save your plot
 }
-
-# now use the gradients package to get the mean, kurtosis, etc and the % 
-# in each standard grain size bucket
-#grain_stats <- granstat(gs_data_matrix)
-
-# set-up the order in which we want grain sizes to be displayed (and which we want to be displayed)
-grain_sizes <- c("Silt",
-                 "VCSilt",
-                 "VFS",
-                 "FS",
-                 "MS",
-                 "CS",
-                 "VCS",
-                 "VFG"
-)
-# Note, leaving off coarse grains
-
-# save your plot
 
 if(sys.nframe() == 0) {
   
   # main program, called via Rscript
-  parser <- ArgumentParser(
+  parser = ArgumentParser(
     prog="Sediment Analysis",
     description="Plot graphs of sediment grain stats for a single core"
   )
@@ -124,6 +123,7 @@ if(sys.nframe() == 0) {
   parser$add_argument('-d', '--depth_file',
                       help="Optional file for depth data")
   
-  args <- parser$parse_args()  
+  args = parser$parse_args()  
   main(args)
 }
+
