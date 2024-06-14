a7583541928660837dd68515cd60def85eb90c93
Julian Wassermann <jw4251@york.ac.uk>
2024-06-14 03:41:52 +0100

    sediment_analysis.R version 5

--- sediment_analysis.R
+++ sediment_analysis.R
@@ -11,17 +11,16 @@ suppressPackageStartupMessages({
 
 # Main body, use call command, figure out how to return a function
 # given a plot, work out its limits
-get_plot_limits <- function(core_16) {
+get_plot_limits <- function(data_exel) {
   # Build ggplot object for the data sets
-  combined_vector <- unlist(core_16)
-  min_value <- min(combined_vector, na.rm = TRUE)
-  max_value <- max(combined_vector, na.rm = TRUE) 
+  min_vale <- min(data_exel)
+  max_value <- max(data_exel)
   
   # Return min and max values
-  return(list(min = min_value, max = max_value))
+  return(min_value, max_value)
 }
 
-read_sed_data <- function(core_16) {
+read_sed_data <- function(depth_file) {
   # Check if the core_16 is a string
   if (!is.character(core_16)) {
     stop("Error: `core_16` must be a string")
@@ -38,9 +37,9 @@ read_core_depths <- function(depth_file) {
   return(data)
 }
 
-calculate_grain_percentages <- function(grain_sizes, grain_stats, gs_data_matrix) {
-  # Placeholder for the actual implementation
-}
+#calculate_grain_percentages <- function(grain_sizes, grain_stats, gs_data_matrix) {
+#Placeholder for the actual implementation
+#}
 
 main <- function(args) {
   if (is.null(args$depth_data)) {
@@ -50,35 +49,35 @@ main <- function(args) {
     depth_file <- args$depth_data 
   }
   
-  grain_stats <- granstat(gs_data_matrix)
+  #grain_stats <- granstat(gs_data_matrix)
   
   # For percentage diagram
   plot1 <- ggplot(grain_stats, aes(x=x, y=y)) + geom_bar(stat = "identity") + xlim(25, 35) + ylim(0, 1)
-  limits1 <- get_plot_limits(plot1)
+  limits1 <- get_plot_limits()
   print("Plot 1 Limits (Percentage Diagram):")
   print(limits1)
   
   # For mean GS (mm) diagram
   plot2 <- ggplot(grain_stats, aes(x=x, y=y)) + geom_line() + xlim(25, 35) + ylim(0, 1.25)
-  limits2 <- get_plot_limits(plot2)
+  limits2 <- get_plot_limits()
   print("Plot 2 Limits (Mean GS (mm) Diagram):")
   print(limits2)
   
   # For kurtosis diagram
   plot3 <- ggplot(grain_stats, aes(x=x, y=y)) + geom_line() + xlim(25, 35) + ylim(0, 10)
-  limits3 <- get_plot_limits(plot3)
+  limits3 <- get_plot_limits()
   print("Plot 3 Limits (Kurtosis Diagram):")
   print(limits3)
   
   # For sorting diagram
   plot4 <- ggplot(grain_stats, aes(x=x, y=y)) + geom_line() + xlim(25, 35) + ylim(0, 10)
-  limits4 <- get_plot_limits(plot4)
+  limits4 <- get_plot_limits()
   print("Plot 4 Limits (Sorting Diagram):")
   print(limits4)
   
   # For skew diagram
   plot5 <- ggplot(grain_stats, aes(x=x, y=y)) + geom_line() + xlim(25, 35) + ylim(0, 10)
-  limits5 <- get_plot_limits(plot5)
+  limits5 <- get_plot_limits()
   print("Plot 5 Limits (Skew Diagram):")
   print(limits5)
   
@@ -92,7 +91,7 @@ main <- function(args) {
 
 # now use the gradients package to get the mean, kurtosis, etc and the % 
 # in each standard grain size bucket
-grain_stats <- granstat(gs_data_matrix)
+#grain_stats <- granstat(gs_data_matrix)
 
 # set-up the order in which we want grain sizes to be displayed (and which we want to be displayed)
 grain_sizes <- c("Silt",
